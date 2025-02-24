b73cddd9144c27df4d618a3eaa3624a40441ab5f
Dip Dutt <emduttt456@gmail.com>
2025-02-24 19:10:02 +0600

    Initial Commit

---
+++ version control/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  version control
+//
+//  Created by Dip Dutt on 24/2/25.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ version control/version_controlApp.swift
@@ -0,0 +1,17 @@
+//
+//  version_controlApp.swift
+//  version control
+//
+//  Created by Dip Dutt on 24/2/25.
+//
+
+import SwiftUI
+
+@main
+struct version_controlApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
